5e4dbb1fb3f8726cc8d09b8e3d369d3d528000c3
Pete Haas <petehaas@petehaas.com>
2020-03-04 17:54:32 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -7,16 +7,16 @@
 const { App } = require('jovo-framework');
 const { Alexa } = require('jovo-platform-alexa');
 const { GoogleAssistant } = require('jovo-platform-googleassistant');
+const { Bixby } = require('jovo-platform-bixby')
 const { JovoDebugger } = require('jovo-plugin-debugger');
 const { FileDb } = require('jovo-db-filedb');
-const { Bixby } = require('jovo-platform-bixby');
 
 const app = new App();
 
 app.use(
-    new Alexa(),
+   // new Alexa(),
+   // new GoogleAssistant(),
     new Bixby(),
-    new GoogleAssistant(),
     new JovoDebugger(),
     new FileDb()
 );
@@ -27,39 +27,56 @@ app.use(
 // ------------------------------------------------------------------
 
 app.setHandler({
-    LAUNCH() {
-        console.log('LAUNCH()');
-        this.ask('Hello World! What\'s your name?', 'Please tell me your name.');
+    
+
+    NEW_USER() {
+        // Triggered when a user opens your app for the first time
+        this.ask(`Welcome, looks like this is your first time here`)
+     },
+     LAUNCH() {
+        return this.toIntent('HelloWorldIntent');
+    },
+
+    CollectInfoIntent() {
+         // location
+         //  dob
+         // 
+
+         if (this.$inputs.dob) {
+            this.$session.$data.dob = this.$inputs.dob.value;
+            this.$session.$data.color = this.$inputs.color.value;
+
+            this.$user.$data.dob = this.$inputs.dob.value;
+            this.$user.$data.color = this.$inputs.color.value;
+            this.ask(`I saved your date of birth, and favorite color`)
+
+         }
+           
+            //this.ask(`Hello there, your location is ${this.$inputs.location.value.point.latitude}`)
+         else 
+             this.ask('Hi there, what is your date of birth?')   
+    },
+
+    HelloWorldIntent() {
+
+        if (this.$user.isNew()) { 
+            this.ask('Welcome, Looks like this is your first time.  Please make a selection');
+        } 
+        else
+          this.ask('Welcome back, you can say hello world, collect some information, or find my location to begin');
+    },
+    LocationIntent() {
+
+
+        this.$session.$data.location = this.$inputs.location.value;
+        this.ask(`Here's your location`)
+
     },
 
     MyNameIsIntent() {
-        console.log('MyNameIsIntent()');
-        this.$bixbyCapsule.$layout.single = 'Hello World.';
         this.$session.$data.name = this.$inputs.name.value;
         this.tell('Hey ' + this.$inputs.name.value + ', nice to meet you!');
     },
-
-    PlayAudioIntent() {
-        console.log('PlayAudioIntent()');
-        this.$bixbyCapsule.$audioPlayer
-            .play({
-                url: 'http://www.testsounds.com/track39.mp3',
-                format: 'audio/mp3'
-            })
-            .setMetaData({
-                id: 'jovo-audio',
-                title: 'Drum Solo',
-                artist: 'Random',
-                albumArtUrl: 'test.png'
-            })
-            .setDisplayName('Jovo Audio - Drum Solo');
-    },
-
-    AudioPlayingIntent() {
-        console.log('AudioPlayingIntent()');
-        this.$layout.single = 'Playing audio - layout style!';
-        this.tell('Playing audio.');
-    }
 });
 
 module.exports.app = app;
